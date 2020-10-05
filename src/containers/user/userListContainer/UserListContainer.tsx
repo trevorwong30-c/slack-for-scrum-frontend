f69a1a100bf5d9116ec1c005bde193c7e567ab0e
Trevor Wong <trevorwong30@gmail.com>
2020-10-06 01:16:13 +0800

    updated: - able to add searched user to tmp list - able to remove user from tmp list - prevent user from confirming a empty member list

--- src/containers/user/userListContainer/UserListContainer.tsx
+++ src/containers/user/userListContainer/UserListContainer.tsx
@@ -5,13 +5,15 @@ import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { loadRequirementList } from '../../../redux/actions/loadRequirementList';
 import {User, UserState} from "../../../interfaces";
-import {FormControl, InputGroup} from "react-bootstrap";
+import {Col, FormControl, InputGroup, Row} from "react-bootstrap";
 import {AsyncTypeahead} from 'react-bootstrap-typeahead';
 import {searchUserByKeyword} from "../../../redux/actions/searchUserByKeyword"; // ES2015
+import "./style.scss";
 
 const UserListContainer = () => {
 
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const [tmpUserList, setTmpUserList] = useState<Array<User>>([]);
 
     const userState:UserState = useSelector(
         (state: RootStateOrAny) => state.user
@@ -28,6 +30,16 @@ const UserListContainer = () => {
     // const closeModal = () => {
     //   setIsModalVisible(false);
     // }
+
+    const addUserToTmpList = (user:User) => {
+        setTmpUserList([...tmpUserList, user]);
+    }
+
+    const removeUserFromTmpList = (idx:number) => {
+        let list = [...tmpUserList];
+        list.splice(idx,1);
+        setTmpUserList(list);
+    }
 
     const onModalClosed = () => {};
 
@@ -58,7 +70,11 @@ const UserListContainer = () => {
     //     }
     // };
 
-    const handleSearch = (keyword:string) => {
+    const confirmUserList = () => {
+
+    }
+
+    const onSearch = (keyword:string) => {
         dispatch(searchUserByKeyword(keyword));
     }
 
@@ -68,6 +84,20 @@ const UserListContainer = () => {
         }
     }
 
+    const renderTmpUserList = () => {
+        return (
+            <div className="tmp-user-list">
+                { tmpUserList.map((user, index) => (
+                    <Row key={`tmpUserList${index}`}>
+                        <Col>{ user.username } ({ user.role })</Col>
+                        <Col>
+                            <button type="button" className="close" onClick={() => removeUserFromTmpList(index)}><span aria-hidden="true">×</span><span
+                                className="sr-only">Close</span></button></Col>
+                    </Row>
+                )) }
+            </div>
+        );
+    }
 
     useEffect(() => {
         showModal();
@@ -79,10 +109,10 @@ const UserListContainer = () => {
     // useEffect(onRequirementListChanged, [requirementState.list]);
 
     return (
-        <div className="ConfirmRequirementContainer">
+        <div className="UserListContainer">
             <Modal show={isModalVisible} onHide={onModalClosed}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Requirement List</Modal.Title>
+                <Modal.Header>
+                    <Modal.Title>Confirm User List</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <AsyncTypeahead
@@ -90,21 +120,22 @@ const UserListContainer = () => {
                         isLoading={userState.isSearching}
                         minLength={1}
                         labelKey="username"
-                        onSearch={handleSearch}
+                        onSearch={onSearch}
                         options={userState.searchResults}
                         placeholder="Search for a user..."
                         renderMenuItemChildren={(option, props) => (
                             <Fragment>
-                                <span>{ option.username }</span>
+                                <span onClick={() => addUserToTmpList(option)}>{ option.username } ({ option.role })</span>
                             </Fragment>
                         )}
                     />
+                    { renderTmpUserList() }
                 </Modal.Body>
                 <Modal.Footer>
                     {/*<Button variant="secondary" onClick={closeModal}>*/}
                     {/*  Close*/}
                     {/*</Button>*/}
-                    <Button variant="primary" onClick={confirmRequirementList}>
+                    <Button variant="primary" onClick={confirmUserList} disabled={tmpUserList.length == 0}>
                         Confirm Users
                     </Button>
                 </Modal.Footer>
